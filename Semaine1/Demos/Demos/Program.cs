b632ced6c081583b260b2da02d6e80a6fea6d5bd
Professeur Cot <martin.couture@uqtr.ca>
2025-01-27 13:33:46 -0500

    Mise à jour convertisseur

--- Semaine1/Demos/Demos/Program.cs
+++ Semaine1/Demos/Demos/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace Demos
+﻿using System.ComponentModel;
+
+namespace Demos
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        static void Main(string[] args) { 
+       Console.WriteLine("Bienvenue au convertisseur Celsius,Kelvin, Fahrenheit");
+            string unite;
+        bool uniteOK = false;
+            do
+            {
+                Console.Write("Choisir l'unité de votre température (C, K, F): ");
+                unite = Console.ReadLine();
+                if (unite != "C" && unite != "K" && unite != "F")
+                {
+                    Console.WriteLine("Unité invalide");
+                }
+                else
+                {
+                    uniteOK = true;
+                }
+            } while (!uniteOK) ;
+
+//Entrez Temperature
+string temperatureString;
+bool estDouble;
+double temperature;
+do
+{
+    Console.Write("Entrez la température: ");
+    temperatureString = Console.ReadLine();
+    estDouble = double.TryParse(temperatureString, out temperature);
+} while (!estDouble);
+
+TemperatureUtil temperatureUtil = new TemperatureUtil();
+
+switch (unite)
+{
+    case "C":
+        Console.WriteLine("La température en Celsius est " + temperature);
+        Console.WriteLine("La température en Fahrenheit est " + temperatureUtil.CelsiusVersFahrenheit(temperature));
+        Console.WriteLine("La température en Kelvin est " + temperatureUtil.CelsiusVersKelvin(temperature));
+        break;
+
+    case "F":
+        Console.WriteLine("La température en Fahrenheit est " + temperature);
+        Console.WriteLine("La température en Celsius est " + temperatureUtil.FahrenheitVersCelsius(temperature));
+        Console.WriteLine("La température en Kelvin est " + temperatureUtil.FahrenheitVersKelvin(temperature));
+        break;
+    case "K":
+        Console.WriteLine("La température en Kelvin est " + temperature);
+        Console.WriteLine("La température en Celsius est " + temperatureUtil.KelvinVersCelsius(temperature));
+        Console.WriteLine("La température en Fahrenheit est " + temperatureUtil.KelvinVersFahreinheit(temperature));
+        break;
+}
+
         }
     }
 }
---
+++ Semaine1/Demos/Demos/TemperatureUtil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    internal class TemperatureUtil
+    {
+        public TemperatureUtil() { }
+
+        public double CelsiusVersFahrenheit(double temp)
+        {
+            return temp * 1.8 + 32;
+        }
+
+        public double CelsiusVersKelvin(double temp)
+        {
+            return temp + 273.15;
+        }
+
+
+        public double FahrenheitVersCelsius(double temp)
+        {
+            return (temp - 32) / 1.8;
+        }
+
+        public double FahrenheitVersKelvin(double temp)
+        {
+            return FahrenheitVersCelsius(temp) + 273.15; ;
+        }
+
+
+        public double KelvinVersCelsius(double temp)
+        {
+            return temp - 273.15;
+        }
+
+        public double KelvinVersFahreinheit(double temp)
+        {
+            return CelsiusVersFahrenheit(KelvinVersCelsius(temp));
+        }
+    }
+}
