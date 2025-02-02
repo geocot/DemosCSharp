3b9d9b63db0b69c5b1852ed9307b3ee698f9bb46
Professeur Cot <martin.couture@uqtr.ca>
2025-02-02 12:52:36 -0500

    Equals et GetHashCode
    Ajout de code

---
+++ Semaine2/Demos/Collections/Personne.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Personne
+    {
+        private string _nom;
+        private string _prenom;
+
+        public Personne(string nom, string prenom)
+        {
+            _nom = nom;
+            _prenom = prenom;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Personne personne &&
+                   _nom == personne._nom &&
+                   _prenom == personne._prenom;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_nom, _prenom);
+        }
+    }
+}
---
+++ Semaine2/Demos/Collections/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne hp = new Personne("Potter", "Harry");
+            Personne hg = new Personne("Potter", "Harry");
+
+            Console.WriteLine(hp.Equals(hg));
+            Console.WriteLine(hp.GetHashCode());
+            Console.WriteLine(hg.GetHashCode());
+        }
+    }
+}
