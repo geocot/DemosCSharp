beef24f17fc0e73b0d4fc891e8d7628fa7697293
Professeur Cot <martin.couture@uqtr.ca>
2025-02-02 16:51:31 -0500

    Dictionnaire et Queue

--- Semaine2/Demos/ExempleCollections/Program.cs
+++ Semaine2/Demos/ExempleCollections/Program.cs
@@ -6,7 +6,33 @@ namespace ExempleCollections
     {
         static void Main(string[] args)
         {
-      
+            //Queue
+            /*Queue<string> villes = new Queue<string>();
+            villes.Enqueue("Montréal");
+            villes.Enqueue("Québec");
+            villes.Enqueue("Trois-Rivières");
+
+            Console.WriteLine($"Le nombre de ville est de {villes.Count()}");
+            Console.WriteLine(villes.Peek());
+            Console.WriteLine($"Le nombre de ville est de {villes.Count()}");
+            Console.WriteLine(villes.Dequeue());
+            Console.WriteLine($"Le nombre de ville est de {villes.Count()}");
+
+            foreach (string ville in villes)
+            {
+                Console.WriteLine(ville);
+            }*/
+
+            //Dictionnaire
+            Dictionary<string,int> villes = new Dictionary<string,int>();
+            villes.Add("Montréal", 1200000);
+            villes.Add("Québec", 600000);
+            villes.Add("Trois-Rivières", 100000);
+
+            foreach (KeyValuePair<string, int> ville in villes)
+            {
+                Console.WriteLine($"Le nom est {ville.Key} et la population est {ville.Value}");
+            }
         }
     }
 }
