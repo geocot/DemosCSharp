ff5ada24f46ff38da30b63ce3e4ac6960b654ff6
Professeur Cot <martin.couture@uqtr.ca>
2025-02-19 12:37:59 -0500

    Semaine 4

---
+++ Semaine4/TestsUnitaires/TestsUnitaires/Calculs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsUnitaires
+{
+    internal class Calculs
+    {
+        public double AireRectangle(double largeur, double longueur)
+        {
+            return largeur + longueur;
+        }
+
+        public double AireCercle(double rayon)
+        {
+            return Math.PI * Math.Pow(rayon, 2);
+        }
+
+       
+    }
+}
---
+++ Semaine4/TestsUnitaires/TestsUnitaires/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestsUnitaires
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculs calculs = new Calculs();
+            Console.WriteLine($"Aire du cercle est {calculs.AireCercle(10)}");
+        }
+    }
+}
