96ace7d4d664bd737158f6c7f3873b64eaaf6ae8
Professeur Cot <martin.couture@uqtr.ca>
2025-02-28 16:07:30 -0500

    Ajout de surcharge d'opérateur

---
+++ Semaine4/SurchargeOperateur/SurchargeOperateur/Point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurchargeOperateur
+{
+    internal class Point
+    {
+        private int _x = 0;
+        private int _y = 0;
+        //Constructeurs
+        public Point() { }
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+        //Accesseurs et mutateurs
+        public int X { get => _x; set => _x = value; }
+        public int Y { get => _y; set => _y = value; }
+        //To String
+        public override string ToString()
+        {
+            return $"La coordonnée en x est {_x} et la coordonnée en y est {_y}";
+        }
+        //Surcharge en +
+        public static Point operator +(Point a, Point b)
+        {
+            Point point = new Point();
+            point.X = a.X + b.X;
+            point.Y = a.Y + b.Y;
+            return point;
+        }
+
+        public static Point operator -(Point a, Point b)
+        {
+            Point point = new Point();
+            point.X = a.X - b.X;
+            point.Y= a.Y - b.Y;
+            return point;
+        }
+    }
+}
---
+++ Semaine4/SurchargeOperateur/SurchargeOperateur/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SurchargeOperateur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(45, -73);
+            Point p2 = new Point(46, -71);
+
+            Point p3 = p1 + p2;
+            Console.WriteLine(p3);
+            Point p4 = p1 - p2;
+            Console.WriteLine(p4);
+
+        }
+    }
+}
